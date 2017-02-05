e7a267505c11bd7d4045f054c229b964bc8a6bf9
r-miyamae <b5005@oic.jp>
2017-02-06 02:06:58 +0900

    temp

--- app/controllers/flash_card_controller.rb
+++ app/controllers/flash_card_controller.rb
@@ -1,10 +1,12 @@
 class FlashCardController < ApplicationController
   def index
-    @user=@current_user
-    userId=current_user.id
+    if @current_user.blank?
+    else
+      @user=@current_user
+      userId=current_user.id
 
-    @userfiles=Userfile.find_by_sql(['select * from userfiles where user_id = :userid',{userid: userId}])
-        p "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+@userfiles.to_s+"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+      @userfiles=Userfile.find_by_sql(['select * from userfiles where user_id = :userid',{userid: userId}])
+    end
 
   end
 end
