7266d07541d3ac117a80f284e50154d5e2ba8e85
s-zenya <b5037@oic.jp>
2017-02-15 16:46:49 +0900

    またしゅうせい

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,16 +29,14 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        redirect_to '/user/sign_in'
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to '/user/sign_in'
+    else
+      format.html { render :new }
+      format.json { render json: @user.errors, status: :unprocessable_entity }
     end
   end
+
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
