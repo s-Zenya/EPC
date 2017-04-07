ff2283b1d5e75cb3d892683e7eb80c6250f436b4
s-zenya <b5037@oic.jp>
2017-04-07 20:27:14 +0900

    shareの検索機能実装

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -1,23 +1,17 @@
 class ShareController < ApplicationController
   def top
-    @userfiles = Userfile.find_by_sql(['select * from release_files order by updated_at'])
+    key=params[:key]
+    if key.present?
+      @userfiles = Userfile.find_by_sql(['select * from release_files where tag = :key order by updated_at',{key: key}])
+    else
+      @userfiles = Userfile.find_by_sql(['select * from release_files order by updated_at'])
+    end
   end
 
   def index
     if current_user.present?
       userId=current_user.id
       @userfiles=Userfile.find_by_sql(['select * from userfiles where user_id = :userid',{userid: userId}])
-    end
-  end
-
-  def search
-    if current_user.present?
-      key = params[:key]
-      p 'hahahahaha'
-      p key
-      userId=current_user.id
-      @userfiles = Userfile.find_by_sql(['select * from release_files where tag = :key order by updated_at',{key: key}])
-      p @userfiles
     end
   end
 
@@ -33,7 +27,6 @@ class ShareController < ApplicationController
   def create
     respond_to do |format|
     @release_file = ReleaseFile.new(params[:release_file])
-    p @release_file
       if @release_file.save
         format.html { redirect_to '/share/create', notice: 'ファイルを公開しました' }
         format.json { redirect_to '/share/create', status: :ok, location: @release_file }
@@ -46,15 +39,11 @@ class ShareController < ApplicationController
   def show
     @release_file = ReleaseFile.find_by(id: params[:id])
     file = Userfile.find_by(id: @release_file.userfiles_id)
-    p file
     @releasing_user = User.find_by(id: file.user_id)
     @userfile = Userfile.new
     @userfile.user_id = session[:user_id]
     @userfile.filename = @release_file.filename
     @user = User.find_by(id: @userfile.user_id)
     @words = Word.where(fileid: @release_file.id)
-    p @user
-    p  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-    p @words
   end
 end
